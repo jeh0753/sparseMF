1d5ef2635923ac0685cf309bc4fc9709e8a004a0
Jake Hawkesworth <jeh0753@gmail.com>
2018-01-11 22:05:00 -0700

    updating versions for pip

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'sparsemf',
   packages = ['sparsemf'], 
-  version = '0.15',
+  version = '0.1',
   description = 'A matrix factorization recommender which runs on top of NumPy and SciPy. Developed with a focus on speed, and highly sparse matrices.',
   author = 'Jake Hawkesworth',
   author_email = 'jeh0753@gmail.com',
   url = 'https://github.com/jeh0753/sparseMF', 
-  download_url = 'https://github.com/jeh0753/sparseMF/archive/0.11.tar.gz', 
+  download_url = 'https://github.com/jeh0753/sparseMF/archive/0.1.tar.gz', 
   keywords = ["imputation", "matrix factorization", "recommender", "recommendation engine", "collaborative filtering", "softimpute"], 
   classifiers = [],
 )
